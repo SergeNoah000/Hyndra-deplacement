815355d64387a35685167d049f170d39715f9a80
Serge Noah <92081418+SergeNoah000@users.noreply.github.com>
2024-05-06 20:09:48 +0100

    Update urls.py

--- hyndra/urls.py
+++ hyndra/urls.py
@@ -22,6 +22,6 @@ import notifications.urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('road.urls'), name="road-app"), 
-    path('^inbox/notifications/', include(notifications.urls, namespace='notifications')),
+    path('inbox/notifications/', include(notifications.urls, namespace='notifications')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
